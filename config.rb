f96991b9fd561f6a2efe6da51bc01f45debdbf77
orionbytes <orion.bytes@gmail.com>
2020-07-15 13:05:56 +0200

    fix for text and remove icons:

--- config.rb
+++ config.rb
@@ -17,6 +17,7 @@ end
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+page '/frolov', layout: 'frolov_layout'
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
